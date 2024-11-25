8c55396f7054f7400bfc96647e07f4f0d58d1edc
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-11-25 10:27:40 +0200

    workspace switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import DottedSeparator from "./dotted-separator";
 import { Navigation } from "./navigation";
+import WorkspaceSwitcher from "./workspace-switcher";
 
 function Sidebar() {
   return (
@@ -9,6 +11,9 @@ function Sidebar() {
       <Link href="/">
         <Image src="/logo.svg" alt="logo" width={164} height={48} />
       </Link>
+
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
 
       <DottedSeparator className="my-4" />
       <Navigation />
