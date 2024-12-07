74cb95c1d0f3cbed2378e0166ba3532a60e16061
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-12-07 17:19:48 +0200

    create task form

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -4,8 +4,10 @@ import DottedSeparator from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { PlusIcon } from "lucide-react";
+import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 
 export default function TaskViewSwitcher() {
+  const { open } = useCreateTaskModal();
   return (
     <Tabs
       defaultValue="tab1"
@@ -26,7 +28,7 @@ export default function TaskViewSwitcher() {
             </TabsTrigger>
           </TabsList>
 
-          <Button size="sm" className="w-full lg:w-auto">
+          <Button onClick={open} size="sm" className="w-full lg:w-auto">
             <PlusIcon className="size-4 mr-2" />
             New
           </Button>
